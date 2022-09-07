fe4c7fed02a4c06fcf1d47318d34834b53353130
Will Pabs <pabiterowilliam@yahoo.com>
2022-09-07 17:10:55 -0400

    Add logic

--- src/App.js
+++ src/App.js
@@ -1,12 +1,48 @@
+import React from "react";
 import Overview from "./components/Overview";
 
-function App() {
-  return (
-    <div className="App">
-      <h2>Welcome to Task App</h2>
-      <Overview title='Task'/>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      tasks: [],
+    };
+
+    this.onButtonSubmit = this.onButtonSubmit.bind(this);
+  };
+
+  onButtonSubmit = () => {
+    const value = document.querySelector('input').value;
+    this.setState({
+      tasks: [...this.state.tasks, value],
+    });
+  }
+  
+  render() {
+    
+
+    return (
+      <div className="App">
+        <h2>Welcome to Task App</h2>
+        <div>
+          <input type="text" />
+          <button onClick={this.onButtonSubmit}>Submit</button>
+        </div>
+        <div className="tasks-list">
+          {this.state.tasks.length > 0 ?
+            this.state.tasks.map((task) => {
+            return (
+              <Overview title={task}/>
+            )}) :
+            (
+              <Overview title='Some task'/>
+            )
+          }
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Overview.js
+++ src/components/Overview.js
@@ -3,21 +3,9 @@
 import React from "react";
 
 class Overview extends React.Component {
-    constructor() {
-        super();
-        
-        this.state = {
-            tasks: []
-        };
-    }
-
     render () {
         return(
             <div className="Overivew">
-                <div>
-                    <input type="text" />
-                    <button onSubmit={this.props.onButtonSubmit}>Submit</button>
-                </div>
                 <h3>{this.props.title}</h3>
             </div>        
         )   
